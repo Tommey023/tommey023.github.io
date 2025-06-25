965f4ef074e42f684a3bf8251563259842adfe2c
tom.vanveen.1994@live.nl <34134210+Tommey023@users.noreply.github.com>
2025-06-25 21:35:02 +0200

    nieuwe webstie

--- js/script.js
+++
@@ -1,5 +0,0 @@
-// Zorg dat deze file staat in: /js/script.js
-document.getElementById('klik-mij').addEventListener('click', function() {
-  const output = document.getElementById('js-output');
-  output.textContent = 'JavaScript werkt 🎉';
-});
